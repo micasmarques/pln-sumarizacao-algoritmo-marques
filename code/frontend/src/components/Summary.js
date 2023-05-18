598743b024e64b290c7c1ea4ca7f929efae00e38
Micael Silva <micael.silva@accountfy.com>
2023-05-17 23:21:16 -0300

    melhorando o front

--- code/frontend/src/components/Summary.js
+++ code/frontend/src/components/Summary.js
@@ -1,38 +1,71 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 function Summary() {
-    const [text, setText] = useState('');
-    const [numSentences, setNumSentences] = useState(1);
-    const [summary, setSummary] = useState('');
+  const [text, setText] = useState('');
+  const [numSentences, setNumSentences] = useState(1);
+  const [summary, setSummary] = useState('');
 
-    const handleClick = () => {
-        fetch('http://localhost:5000/summarize', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                text,
-                num_sentences: numSentences
-            })
-        })
-            .then(response => response.json())
-            .then(data => {
-                setSummary(data.summary);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    };
+  const handleClick = () => {
+    const parsedNumSentences = parseInt(numSentences, 10);
+    fetch('http://localhost:5000/summarize', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        text,
+        num_sentences: parsedNumSentences
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        setSummary(data.summary);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
 
-    return (
-        <div>
-            <textarea value={text} onChange={(e) => setText(e.target.value)}/>
-            <input type="number" value={numSentences} onChange={(e) => setNumSentences(e.target.value)}/>
-            <button onClick={handleClick}>Summarize</button>
-            <p>{summary}</p>
-        </div>
-    );
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+        gap: 2
+      }}
+    >
+      <Typography variant="h4" component="div" gutterBottom>
+        Text Summarization
+      </Typography>
+      <TextField
+        variant="outlined"
+        multiline
+        rows={4}
+        fullWidth
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        label="Enter Text"
+      />
+      <TextField
+        variant="outlined"
+        type="number"
+        value={numSentences}
+        onChange={(e) => setNumSentences(parseInt(e.target.value))}
+        label="Number of Sentences"
+      />
+      <Button variant="contained" onClick={handleClick}>
+        Summarize
+      </Button>
+      <Typography variant="body1">{summary}</Typography>
+    </Box>
+  );
 }
 
 export default Summary;
